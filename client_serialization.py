8a359a5d88e1bb1eb4a862b84b3fab96a4166bda
glebreutov <glebr@algotradinghub.com>
2017-03-28 12:01:03 +0300

    client fix

--- client_serialization.py
+++ client_serialization.py
@@ -28,7 +28,7 @@ def serialize_pnl(pnl: PNL):
     return json.dumps({"e": "pnl", "details": {'position': str(pnl.position()),
                        'balance': str(pnl.balance()),
                        'last traded price': str(pnl.last_traded_price()),
-                       'closed PNL': pnl.closed_pnl}})
+                       'closed PNL': str(pnl.closed_pnl)}})
 
 
 def serialize_execs(execs):
